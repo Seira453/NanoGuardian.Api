1435487dbf78959af33714039475c92325294991
brend <brend@DESKTOP-ELNO4QI>
2026-04-13 23:26:17 -0600

    Agregar archivos de proyecto.

---
+++ Controllers/AlertasController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NanoGuardian.Api.Models;
+
+namespace NanoGuardian.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AlertasController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult RecibirAlerta([FromBody] AlertaZapato alerta)
+        {
+            if (string.IsNullOrEmpty(alerta.Usuario))
+            {
+                return BadRequest("El nombre del usuario es obligatorio.");
+            }
+
+            // Simulación del backend
+            Console.WriteLine($"🚨 ALERTA ZAPATO BIÓNICO:");
+            Console.WriteLine($"Usuario: {alerta.Usuario}");
+            Console.WriteLine($"Impacto: {alerta.Impacto}");
+            Console.WriteLine($"Estado: {alerta.Estado}");
+
+            return Ok(new
+            {
+                mensaje = "Alerta del zapato procesada correctamente",
+                codigo = 200
+            });
+        }
+    }
+}
---
+++ Models/AlertaZapato.cs
@@ -0,0 +1,9 @@
+﻿namespace NanoGuardian.Api.Models
+{
+    public class AlertaZapato
+    {
+        public required string Usuario { get; set; }
+        public int Impacto { get; set; } // Nivel de caída
+        public string Estado { get; set; } = "Caida Detectada";
+    }
+}
